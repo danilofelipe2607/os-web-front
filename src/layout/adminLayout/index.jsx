5d6c5499f1c432d5a4939c5cdb482fcbcc26587e
danilo <danilofelipe2607@gmail.com>
2020-01-21 16:57:33 -0300

    criado página de login

--- src/layout/adminLayout/index.jsx
+++ src/layout/adminLayout/index.jsx
@@ -1,99 +1,46 @@
 import React from "react";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardGroup,
-  Col,
-  Container,
-  Form,
-  Input,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Row
-} from "reactstrap";
 
+import { Button } from "primereact/button";
+import logo from "../../assets/img/logo.png";
+
+import "./style.css";
 export default function AdminLayout() {
   return (
-    <div className="app flex-row align-items-center">
-      <Container>
-        <Row className="justify-content-center">
-          <Col md="8">
-            <CardGroup>
-              <Card className="p-4">
-                <CardBody>
-                  <Form>
-                    <h1>Login</h1>
-                    <p className="text-muted">Entre com sua Conta</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        type="text"
-                        placeholder="Username"
-                        autoComplete="username"
-                      />
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                      />
-                    </InputGroup>
-                    <Row>
-                      <Col xs="6">
-                        <Button color="primary" className="px-4">
-                          Login
-                        </Button>
-                      </Col>
-                      <Col xs="6" className="text-right">
-                        <Button color="link" className="px-0">
-                          Forgot password?
-                        </Button>
-                      </Col>
-                    </Row>
-                  </Form>
-                </CardBody>
-              </Card>
-              <Card
-                className="text-white bg-primary py-5 d-md-down-none"
-                style={{ width: "44%" }}
-              >
-                <CardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua.
-                    </p>
-                    {/* <Link to="/register">
-                      <Button
-                        color="primary"
-                        className="mt-3"
-                        active
-                        tabIndex={-1}
-                      >
-                        Register Now!
-                      </Button>
-                    </Link> */}
-                  </div>
-                </CardBody>
-              </Card>
-            </CardGroup>
-          </Col>
-        </Row>
-      </Container>
+    <div className="container">
+      <div className="box">
+        <div className="login-body">
+          <div className="login-panel p-fluid">
+            <div className="login-panel-header" style={{ textAlign: "center" }}>
+              <img src={logo} style={{ width: "80px", height: "70px" }} />
+            </div>
+            <div className="login-panel-content">
+              <div className="p-grid">
+                <div className="p-col-12" style={{ textAlign: "center" }}>
+                  <h1>OS-Web</h1>
+                </div>
+                <div className="p-col-12" style={{ textAlign: "center" }}>
+                  <span className="md-inputfield">
+                    <input className="p-inputtext p-component" />
+                    <label>Email</label>
+                  </span>
+                </div>
+                <div className="p-col-12" style={{ textAlign: "center" }}>
+                  <span className="md-inputfield">
+                    <input className="p-inputtext p-component" />
+                    <label>Senha</label>
+                  </span>
+                </div>
+                <div className="p-col-12">
+                  <Button className="p-button-warning" label="Entrar" />
+                </div>
+                <div className="p-col-12">
+                  Não possui Conta? <a href="/#">Cadastre-se</a>.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
