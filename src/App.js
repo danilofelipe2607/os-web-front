b4965e5c06559dbb0c3411ea3c04790fe08605c6
Danilo <danilofelipe2607@gmail.com>
2020-01-21 14:47:59 -0300

    criando layout admin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import DefaultLayout from "../src/layout/defaultLayout/index";
+import AdminLayout from "../src/layout/adminLayout/index";
 import { HashRouter } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
 export default function App() {
   return (
     <HashRouter>
       <ScrollToTop>
-        <DefaultLayout />
+        <AdminLayout />
       </ScrollToTop>
     </HashRouter>
   );
